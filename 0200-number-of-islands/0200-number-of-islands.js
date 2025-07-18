4bd5674e34fb85189010e65bb0743e959ca3f036
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-07-17 20:47:31 -0400

    Time: 67 ms (44.46%), Space: 63.1 MB (41.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.js
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -5,30 +5,26 @@
 var numIslands = function(grid) {
     const m = grid.length;
     const n = grid[0].length;
+
     let count = 0;
 
     const bfs = (i, j) => {
         const queue = [[i, j]];
-        grid[i][j] = '0';
 
-         while(queue.length > 0){
+        while(queue.length > 0){
             const [x, y] = queue.shift();
 
             for(const [dx, dy] of [[0, 1], [1, 0], [0, -1], [-1, 0]]){
                 const nx = x + dx, ny = y + dy;
-                if(
-                    nx >= 0 && nx < m &&
-                    ny >= 0 && ny < n &&
-                    grid[nx][ny] === '1'
-                ){
+                if(nx >= 0 && nx < m &&
+                ny >= 0 && ny < n &&
+                grid[nx][ny] === '1'){
                     grid[nx][ny] = '0';
                     queue.push([nx, ny]);
-                 }
+                }
             }
-         }
+        }
     };
-
-
 
     for(let i=0; i<m; i++){
         for(let j=0; j<n; j++){
