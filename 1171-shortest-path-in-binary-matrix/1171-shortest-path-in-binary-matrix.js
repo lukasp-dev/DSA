3051f60a738453ee4038b03d9f0795debdad30e2
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-07-17 21:48:09 -0400

    Time: 40 ms (81.85%), Space: 64.3 MB (74.73%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.js
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.js
@@ -4,31 +4,36 @@
  */
 var shortestPathBinaryMatrix = function(grid) {
     const n = grid.length;
-    if(grid[0][0] == 1 || grid[n-1][n-1] == 1){
+    if(grid[0][0] === 1 || grid[n-1][n-1] === 1){
         return -1;
     }
+    
+    const dirs = [[1, 0], [0, 1], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]];
+    let dist = 1;
 
-    const dirs = [[0,1], [1,0], [-1, 0], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]];
-
-    const queue = [[0, 0, 1]];
+    const queue = [[0, 0]];
     grid[0][0] = 1;
-
+    
     while(queue.length > 0){
-        const [x, y, dist] = queue.shift();
-        if(x === n-1 && y === n-1){
-            return dist;
-        }
+        const size = queue.length;
 
-        for(const [dx, dy] of dirs){
-            const nx = x + dx, ny = y + dy;
+        for(let i=0; i<size; i++){
+            const [x, y] = queue.shift();
+            if(x === n-1 && y === n-1){
+                return dist;
+            } 
 
-            if(nx >= 0 && nx < n &&
-            ny >= 0 && ny < n &&
-            grid[nx][ny] === 0){
-                grid[nx][ny] = 1;
-                queue.push([nx, ny, dist+1]);
+            for(const [dx, dy] of dirs){
+                const nx = x + dx, ny = y + dy;
+                if(nx >= 0 && nx < n &&
+                ny >= 0 && ny < n &&
+                grid[nx][ny] === 0){
+                    grid[nx][ny] = 1;
+                    queue.push([nx, ny]);
+                }
             }
-        }
+        } 
+        dist++;
     }
 
     return -1;
