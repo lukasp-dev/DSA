c22fe4caec2df6c5903c3a9471837393bc4edd69
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-07-20 14:31:21 -0400

    Time: 0 ms (100%), Space: 40.3 MB (57.85%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
+        vector<int> v(nums.size());
+        int prefix = 1, postfix = 1;
+        v[0] = prefix;
 
-        vector<int> left(n, 1);
-        vector<int> right(n, 1);
-        vector<int> ret(n,0);
-
-        int mult = 1;
-
-        for(int i=0; i<n; i++){
-            left[i] = nums[i]*mult;
-            mult = left[i];
+        for(int i=1; i<nums.size(); i++){
+            prefix *= nums[i-1];
+            v[i] = prefix;
         }
 
-        mult = 1;
-
-        for(int i=n-1; i>=0; i--){
-            right[i] = nums[i]*mult;
-            mult = right[i];
-        }
-
-        for(int i=0; i<n; i++){
-            if(i == 0){
-                ret[i] = right[i+1];
-            } else if(i == n-1){
-                ret[i] = left[i-1];
-            } else{
-                ret[i] = left[i-1]*right[i+1];
-            }
+        for(int i=nums.size()-2; i>=0; i--){
+            postfix *= nums[i+1];
+            v[i] = v[i]*postfix;
         }
         
-        return ret;
+
+        return v;
     }
 };
