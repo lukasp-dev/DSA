c7f2244d6f88364eacfc3249fe05015b8fbf73c8
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-07-20 19:53:35 -0400

    Time: 0 ms (100%), Space: 10.8 MB (6.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string str1, str2;
-        for(int i=0; i<s.size(); i++){
-            char now = s[i];
-            if(isalnum(now)) str1+= tolower(now);
+        string str;
+        for(char c : s){
+            if(isspace(c) || !isalnum(c)) continue;
+            str += tolower(c);
         }
-        str2 = str1;
-        reverse(str1.begin(), str1.end());
-        return str1 == str2;
+
+        string tmp = str;
+        reverse(str.begin(), str.end());
+
+        return tmp == str;
     }
 };
