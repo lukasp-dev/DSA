5f8bf3696c190213d6848c992fa9790e9ed85135
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-07-19 23:48:47 -0400

    Time: 55 ms (38.48%), Space: 90.9 MB (46.46%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> um;
+        for(int i=0; i<nums.size(); i++){
+            if(++um[nums[i]] > 1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
