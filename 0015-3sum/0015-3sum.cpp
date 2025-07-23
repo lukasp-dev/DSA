3e3e9bb2c023d108ded0a635f9a47a91fee2b07a
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-07-22 22:59:11 -0400

    Time: 36 ms (97.37%), Space: 29.1 MB (72.74%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -15,7 +15,6 @@ public:
         sort(nums.begin(), nums.end());      
 
         for(int i=0; i<nums.size(); i++){
-
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             int curr = nums[i];
             
