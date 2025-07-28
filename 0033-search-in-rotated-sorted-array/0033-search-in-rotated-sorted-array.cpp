de86f3a57b35d2dc735678336497ae3646e506af
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-07-28 15:59:59 -0400

    Time: 0 ms (100%), Space: 15 MB (94.86%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0, r = nums.size() - 1;
+        int l = 0, r = nums.size()-1;
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
+        while(l <= r){
+            int mid = l + (r-l)/2;
 
-            if (nums[mid] == target) return mid;
-
-            // 오른쪽이 정렬된 경우
-            if (nums[mid] < nums[r]) {
-                if (nums[mid] < target && target <= nums[r]) {
+            if(nums[mid] == target){
+                return mid;
+            } else if(nums[mid] < nums[r]){
+                if(nums[mid] <= target && target <= nums[r]){
                     l = mid + 1;
-                } else {
+                }else{
                     r = mid - 1;
                 }
-            }
-            // 왼쪽이 정렬된 경우
-            else {
-                if (nums[l] <= target && target < nums[mid]) {
+            }else{
+                if(nums[l] <= target && target <= nums[mid]){
                     r = mid - 1;
-                } else {
+                }else{
                     l = mid + 1;
                 }
             }
