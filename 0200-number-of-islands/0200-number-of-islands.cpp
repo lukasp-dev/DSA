2355855d1cb5e51dcd14d5850ccb4a674c2010e3
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-08-08 15:38:06 -0400

    Time: 29 ms (45.14%), Space: 24.5 MB (17.13%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -37,6 +37,7 @@ public:
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(!visited[i][j] && grid[i][j] == '1'){
+                    visited[i][j] = 1;
                     bfs(grid, visited, i, j, m, n);
                     count++;
                 }
