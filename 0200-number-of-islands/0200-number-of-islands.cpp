bf7813ee5d725cc72089f83060b3adda71ccf29c
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-08-11 18:34:06 -0400

    Time: 31 ms (32.47%), Space: 17.4 MB (51.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    vector<int> dx = {0, -1, 0, 1};
     vector<int> dy = {-1, 0, 1, 0};
+    vector<int> dx = {0, 1, 0, -1};
+    const int DIR = 4;
 
-    void bfs(vector<vector<char>>& grid, vector<vector<int>>& visited, int y, int x, int m, int n){
-        queue<pair<int, int>> q;
-        q.push({y, x});
+    void dfs(vector<vector<char>>& grid, vector<vector<int>>& visited, int y, int x, int m, int n){
+        visited[y][x] = 1;
+        
+        for(int i=0; i<DIR; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
 
-        while(!q.empty()){
-            pair<int, int> curr = q.front(); q.pop();
-            for(int i=0; i<4; i++){
-                int ny = curr.first + dy[i];
-                int nx = curr.second + dx[i];
-
-                if(ny < 0 || ny >=m || nx < 0 || nx >= n) continue;
-                if(!visited[ny][nx] && grid[ny][nx] == '1'){
-                    visited[ny][nx] = 1;
-                    q.push({ny, nx});
-                }
+            if(ny < 0 || ny >= m || nx < 0 || nx >= n) continue;
+            if(grid[ny][nx] == '1' && !visited[ny][nx]){
+                dfs(grid, visited, ny, nx, m, n);
             }
         }
     }
@@ -28,12 +24,12 @@ public:
         int count = 0;
 
         vector<vector<int>> visited(m, vector<int>(n, 0));
-        
+
+        // 끊어진 곳이 있을 수도 있으므로
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(!visited[i][j] && grid[i][j] == '1'){
-                    visited[i][j] = 1;
-                    bfs(grid, visited, i, j, m, n);
+                    dfs(grid, visited, i, j, m, n);
                     count++;
                 }
             }
