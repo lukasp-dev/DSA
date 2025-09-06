3dd756e9ba6f8573ce955b9394006167fbbab947
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-09-06 16:22:13 -0400

    Time: 0 ms (100%), Space: 17.1 MB (69.27%) - LeetArchive

---
+++ LeetCode/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ret = 0;
+
+        for(int i=0; i<prices.size()-1; i++){
+            int curr = prices[i];
+            int next = prices[i+1];
+            
+            if(curr < next){
+                ret += (next - curr);
+            }
+        }
+
+        return ret;
+    }
+};
