a75bd0adeb0cb73a7ede81846a682f2358475eda
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-10-11 17:54:05 -0400

    Time: 0 ms (100%), Space: 8.5 MB (66.6%) - LeetArchive

---
+++ LeetCode/0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int l = 0, r = 0;
+        int n = s.size(), m = t.size();
+
+        while(l<n && r<m){
+            if(s[l] == t[r]){
+                l++; r++;
+            }else{
+                r++;
+            }
+        }
+
+        return l == n;
+    }
+};
