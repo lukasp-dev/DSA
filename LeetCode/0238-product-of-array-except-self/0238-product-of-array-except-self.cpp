6f092561dd68d5dd042a0c708c97966bb5fcb999
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-10-10 23:30:26 -0400

    Time: 0 ms (100%), Space: 42 MB (31.93%) - LeetArchive

---
+++ LeetCode/0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix(n);
+        vector<int> suffix(n);
+        vector<int> answer(n);
+
+        prefix[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] * nums[i];
+        }
+
+        suffix[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] * nums[i];
+        }
+
+        // Construct answer
+        answer[0] = suffix[1];
+        answer[n - 1] = prefix[n - 2];
+        for (int i = 1; i < n - 1; i++) {
+            answer[i] = prefix[i - 1] * suffix[i + 1];
+        }
+
+        return answer;
+    }
+};
