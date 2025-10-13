5376016f62d96b7d690e3caf7cdd0dd67f8e6610
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-10-13 10:05:06 -0400

    Time: 33 ms (28.49%), Space: 50.5 MB (14.97%) - LeetArchive

---
+++ LeetCode/2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        int dy[] = {-1, 0, 1, 0};
+        int dx[] = {0, -1, 0, 1};
+
+        int n = maze.size(), m = maze[0].size();
+
+        vector<vector<int>> visited(n, vector<int>(m, -1));
+
+        queue<vector<int>> q;
+        visited[entrance[0]][entrance[1]] = 0;
+        q.push(entrance);
+
+        while(!q.empty()){
+            vector<int> curr = q.front(); q.pop();
+            int currY = curr[0], currX = curr[1];
+           if ((currY == 0 || currY == n-1 || currX == 0 || currX == m-1) 
+                && !(currY == entrance[0] && currX == entrance[1])) {
+                return visited[currY][currX];
+            }
+
+
+            for(int i=0; i<4; i++){
+                int ny = currY + dy[i], nx = currX + dx[i];
+
+                if(ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+
+                if(visited[ny][nx] == -1 && maze[ny][nx] == '.'){
+                    q.push({ny, nx});
+                    visited[ny][nx] = visited[currY][currX]+1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
