7d579f5992c6867d59b476062664789c129ff75f
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-10-15 13:11:14 -0400

    Time: 92 ms (45.02%), Space: 182.2 MB (31.68%) - LeetArchive

---
+++ LeetCode/0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,30 @@
+class LRUCache {
+public:
+    int capacity;
+    list<int> order;
+    unordered_map<int, pair<list<int>::iterator, int>> lookup;
+
+    LRUCache(int capacity) : capacity(capacity) {}
+
+    int get(int key) {
+        auto it = lookup.find(key);
+        if (it == lookup.end()) return -1;
+        int value = it->second.second;
+        order.erase(it->second.first);
+        order.push_front(key);
+        it->second.first = order.begin();
+        return value;
+    }
+
+    void put(int key, int value) {
+        auto it = lookup.find(key);
+        if (it != lookup.end()) order.erase(it->second.first);
+        order.push_front(key);
+        lookup[key] = {order.begin(), value};
+        if ((int)order.size() > capacity) {
+            int oldKey = order.back();
+            order.pop_back();
+            lookup.erase(oldKey);
+        }
+    }
+};
