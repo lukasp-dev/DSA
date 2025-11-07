4d75e4d4d26daef818b8f5aac4629bada8929e6f
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-11-07 15:05:17 -0500

    Time: 7 ms (44.45%), Space: 15.9 MB (5.95%) - LeetArchive

---
+++ LeetCode/0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> indices;
+        
+        for(int i=0; i<nums.size(); i++){
+            indices[nums[i]] = i;
+        }
+
+        for(int i=0; i<nums.size(); i++){
+            int complement = target - nums[i];
+
+            if(indices.find(complement) != indices.end() && i != indices[complement]){
+                return {i, indices[complement]};
+            }
+        }
+
+        return {};
+    }
+};
