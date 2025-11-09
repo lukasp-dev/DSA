fca7f37a872e2af1fdd1ab3684eedff907113c3d
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-11-08 21:47:52 -0500

    Time: 0 ms (100%), Space: 12.1 MB (58.75%) - LeetArchive

---
+++ LeetCode/0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int n = board.size(), m = board[0].size();
+        int count = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(board[i][j] == 'X'){
+                    if(i > 0 && board[i-1][j] == 'X') continue;
+                    if(j > 0 && board[i][j-1] == 'X') continue;
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
