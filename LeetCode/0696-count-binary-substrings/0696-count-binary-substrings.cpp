7e6035eba5250aea79965235d1c500275a522c60
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-11-09 00:25:08 -0500

    Time: 0 ms (100%), Space: 13.1 MB (63.69%) - LeetArchive

---
+++ LeetCode/0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+        int res = 0;
+        int prev = 0;
+        int curr = 1;
+
+        for(int i=1; i<s.size(); i++){
+            if(s[i] == s[i-1]){
+                curr++;
+            }else{
+                res += min(prev, curr);
+                prev = curr;
+                curr = 1;
+            }
+        }
+
+        res += min(prev, curr);
+        return res;
+    }
+};
