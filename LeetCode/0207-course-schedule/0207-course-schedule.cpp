1414f451e8046ee105f170225abc6aeae7cc8057
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-11-09 20:24:04 -0500

    Time: 0 ms (100%), Space: 19.3 MB (62.25%) - LeetArchive

---
+++ LeetCode/0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& adj, vector<int>& status, int node){
+        if(status[node] == 1) return false;
+        if(status[node] == 2) return true;
+
+        status[node] = 1;
+
+        for(int nextNode : adj[node]){
+            if(!dfs(adj, status, nextNode)) return false;
+        }
+
+        status[node] = 2;
+        return true;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> status(numCourses, 0);
+
+        for(vector<int>& pre : prerequisites){
+            adj[pre[1]].push_back(pre[0]);
+        }
+
+        for(int i=0; i<numCourses; i++){
+            if(!dfs(adj, status, i)) return false;
+        }
+
+        return true;
+    }
+};
