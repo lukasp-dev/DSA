31064c0fc89897fb963ca69363ac5317cb35808b
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2025-11-10 23:22:23 -0500

    Time: 2 ms (46.68%), Space: 10.2 MB (79.58%) - LeetArchive

---
+++ LeetCode/0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isVowel(char c){
+        c = tolower(c);
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+            return true;
+        }
+        return false;
+    }
+
+    string reverseVowels(string s) {
+        int l = 0;
+        int r = s.size()-1;
+
+        while(l < r){
+            if(isVowel(s[l]) && isVowel(s[r])){
+                swap(s[l], s[r]);
+                l++;r--;
+            }
+            if(!isVowel(s[l])){
+                l++;
+            }
+            if(!isVowel(s[r])){
+                r--;
+            }
+        }
+
+        return s;
+    }
+};
