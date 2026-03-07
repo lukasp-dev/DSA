8125d7d6d72f6704c58be1e69ea6701a574facfd
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2026-03-07 13:06:08 -0500

    Time: 52 ms (40.41%), Space: 90.9 MB (44.05%) - LeetArchive

---
+++ LeetCode/0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        
+        for(int num : nums){
+            freq[num]++;
+            if(freq[num] > 1){
+                return true;
+                break;
+            }
+        }
+
+        return false;
+    }
+};
