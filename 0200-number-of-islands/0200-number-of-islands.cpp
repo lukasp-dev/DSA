dcca4dbc09cd8cc908a5b303034bd1c4f6791b8c
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2026-04-08 14:28:16 -0400

    Time: 19 ms (96.65%), Space: 17.4 MB (46%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,36 +1,28 @@
 class Solution {
-public:
-    vector<int> dy = {-1, 0, 1, 0};
-    vector<int> dx = {0, 1, 0, -1};
-    const int DIR = 4;
+vector<int> dy = {-1, 0, 1, 0};
+vector<int> dx = {0, 1, 0, -1};
 
+public:
     void dfs(vector<vector<char>>& grid, vector<vector<int>>& visited, int y, int x, int m, int n){
         visited[y][x] = 1;
-        
-        for(int i=0; i<DIR; i++){
-            int ny = y + dy[i];
-            int nx = x + dx[i];
 
-            if(ny < 0 || ny >= m || nx < 0 || nx >= n) continue;
-            if(grid[ny][nx] == '1' && !visited[ny][nx]){
-                dfs(grid, visited, ny, nx, m, n);
-            }
+        for(int i=0; i<4; ++i){
+            int ny = y + dy[i], nx = x + dx[i];
+            if(ny < 0 || ny >= m || nx < 0 || nx >= n || visited[ny][nx] || grid[ny][nx] == '0') continue;
+            dfs(grid, visited, ny, nx, m, n);
         }
     }
-
+    
     int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> visited(m, vector<int>(n, 0));
         int count = 0;
 
-        vector<vector<int>> visited(m, vector<int>(n, 0));
-
-        // 끊어진 곳이 있을 수도 있으므로
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(!visited[i][j] && grid[i][j] == '1'){
+        for(int i=0; i<m; ++i){
+            for(int j=0; j<n; ++j){
+                if(grid[i][j] == '1' && !visited[i][j]){
                     dfs(grid, visited, i, j, m, n);
-                    count++;
+                    ++count;
                 }
             }
         }
