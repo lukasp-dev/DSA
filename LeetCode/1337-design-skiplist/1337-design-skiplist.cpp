60264be78331fa3dce0bd29041d5da439824367f
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2026-07-09 18:24:47 -0700

    Time: 52 ms (64.18%), Space: 82.7 MB (21.08%) - LeetArchive

---
+++ LeetCode/1337-design-skiplist/1337-design-skiplist.cpp
@@ -0,0 +1,109 @@
+class Skiplist {
+private:
+    struct Node {
+        int val;
+        vector<Node*> next;
+
+        Node(int v, int level):val(v), next(level, nullptr){}
+    };
+    
+    Node* head;
+    int currentLevel;
+    static int constexpr MAX_LEVEL = 32;
+
+public:
+    int getRandomLevel(){
+        static random_device rd;
+        static mt19937 gen(rd());
+        static uniform_int_distribution dist(0, 1);
+
+        int level = 1;
+
+        while(level < MAX_LEVEL && dist(gen) == 1){
+            ++level;
+        }
+
+        return level;
+    }
+
+    Skiplist(){
+        currentLevel = 1;
+        head = new Node(INT_MAX, MAX_LEVEL);
+    }
+    
+    bool search(int target) {
+        Node* cur = head;
+
+        for(int level = currentLevel - 1; level >= 0; --level){
+            while(cur->next[level] && cur->next[level]->val < target){
+                cur = cur->next[level];
+            }
+        }
+
+        cur = cur->next[0];
+
+        return cur && cur->val == target;
+    }
+    
+    void add(int num) {
+        Node* cur = head;
+        vector<Node*> update(MAX_LEVEL, nullptr);
+
+        for(int level = currentLevel - 1; level >= 0; --level){
+            while(cur->next[level] && cur->next[level]->val < num){
+                cur = cur->next[level];
+            }
+            update[level] = cur;
+        }
+
+        int randomLevel = getRandomLevel();
+
+        if(randomLevel > currentLevel){
+            for(int level = currentLevel; level < randomLevel; ++level){
+                update[level] = head;
+            }
+            currentLevel = randomLevel;
+        }
+
+        Node* newNode = new Node(num, randomLevel);
+
+        for(int level = 0; level < randomLevel; ++level){
+            newNode->next[level] = update[level]->next[level];
+            update[level]->next[level] = newNode;
+        }
+    }
+    
+    bool erase(int num) {
+        Node* cur = head;
+        vector<Node*> update(MAX_LEVEL, nullptr);
+
+        for(int level = currentLevel - 1; level >= 0; --level){
+            while(cur->next[level] && cur->next[level]->val < num){
+                cur = cur->next[level];
+            }
+            update[level] = cur;
+        }
+
+        Node* node_to_delete = cur->next[0];
+        if(!node_to_delete || node_to_delete->val != num) return false;
+
+        for(int level = currentLevel - 1; level >= 0; --level){
+            if(update[level]->next[level] != node_to_delete) continue;
+            update[level]->next[level] = node_to_delete->next[level];
+        }
+
+        delete node_to_delete;
+
+        while(head->next[currentLevel - 1] == nullptr) --currentLevel;
+
+        return true;
+    }
+};
+
+/**
+ * Your Skiplist object will be instantiated and called as such:
+ * Skiplist* obj = new Skiplist();
+ * bool param_1 = obj->search(target);
+ * obj->add(num);
+ * bool param_3 = obj->erase(num);
+ */
