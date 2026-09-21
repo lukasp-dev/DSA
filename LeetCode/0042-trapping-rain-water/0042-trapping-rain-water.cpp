5cbb61be3a1e132dac6b0030ac3430f87ca4f1d6
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2026-09-21 16:09:13 -0400

    Time: 0 ms (100%), Space: 26 MB (55.55%) - LeetArchive

---
+++ LeetCode/0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l = 0, r = height.size()-1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int ans = 0;
+        
+        while(l < r) {
+            if(height[l] < height[r]) {
+                leftMax = max(leftMax, height[l]);
+                ans += leftMax - height[l];
+                l++;
+            } else {
+                rightMax = max(rightMax, height[r]);
+                ans += rightMax - height[r];
+                r--;
+            }
+        }
+
+        return ans;
+    }
+};
