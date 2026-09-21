5e9fcbf16e79b429afc7ecb262ef14f44f809142
Jewook Park <91437843+lukasp-dev@users.noreply.github.com>
2026-09-20 22:17:24 -0400

    Time: 0 ms (100%), Space: 23.9 MB (55.31%) - LeetArchive

---
+++ LeetCode/0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int k=0;
+
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] != 0) {
+                nums[k++] = nums[i];
+            }
+        }
+
+        for(; k<nums.size(); k++) {
+            nums[k] = 0;
+        }
+    }
+};
